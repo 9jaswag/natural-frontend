ba9795df01086b5840b7e7c62e831bae426f8889
Lalalalala <manu@korfmann.info>
2018-04-09 22:54:11 +0200

    save

--- app/models/row.js
+++ app/models/row.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  table: DS.belongsTo('table')
+  table: DS.belongsTo('table'),
+  name: DS.attr('string'),
+  type: DS.attr('string')
 });
