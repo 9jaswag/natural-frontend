2ca39b86c623d7bdfa57435b03242a95b5b3a86f
Lalalalala <manu@korfmann.info>
2018-04-12 05:08:34 +0200

    added copyright notice

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
+// Copyright (C) Manuel Arno Korfmann - All Rights Reserved
+// Unauthorized copying of this file, or parts of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// Written by Manuel Arno Korfmann <manu@korfmann.info>, April 2018
+
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
