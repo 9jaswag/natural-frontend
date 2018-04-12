7a42b451dcbcc170c02037b93c3887552710849b
Lalalalala <manu@korfmann.info>
2018-04-12 18:42:55 +0200

    remove copyright notice

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,3 @@
-// Copyright (C) Manuel Arno Korfmann - All Rights Reserved
-// Unauthorized copying of this file, or parts of this file, via any medium is strictly prohibited
-// Proprietary and confidential
-// Written by Manuel Arno Korfmann <manu@korfmann.info>, April 2018
 import ENV from "../config/environment";
 import DS from 'ember-data';
 import { pluralize } from 'ember-inflector';
--- testem.js
+++ testem.js
@@ -1,7 +1,3 @@
-module.exports = {
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
     'Chrome'
   ],
   launch_in_dev: [
