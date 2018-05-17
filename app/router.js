269334c8fafa0218718cc017c5a4be554fa61c5b
Lalalalala <manu@korfmann.info>
2018-05-17 20:19:54 +0200

    wip

--- app/router.js
+++ app/router.js
@@ -11,7 +11,9 @@ Router.map(function() {
   this.route('projects');
   this.route('databases', { path: 'projects/:project_id/databases' });
   this.route('tables', { path: 'databases/:database_id/tables' });
-  this.route('table', { path: 'tables/:table_id' });
+  this.route('table', { path: 'tables/:table_id' }, function() {
+    this.route('row-value', { path: 'row-value/:rowValueId' });
+  });
   this.route('authenticate');
 });
 
---
+++ app/routes/row-value.js
@@ -0,0 +1,9 @@
+
+import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from '../mixins/authenticated-route-mixin';
+
+export default Route.extend(AuthenticatedRouteMixin, {
+  model(params) {
+    return this.get('store').findRecord('table', params.table_id);
+  }
+});
